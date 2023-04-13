08dd419bcd45b6e1ef5af5750867ebc949588775
Anushka Srivastava <anushka-srivastava22>
2023-04-14 03:49:52 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,48 @@
+// 1. Import the extendTheme function
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar';
+import './App.css'
+import HomePage from './components/Home/HomePage';
+import Footer from './components/Footer';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Trivia from './components/TechTrivia/Trivia';
+import Dashboard from './components/Dashboard/Dashboard';
+import NotesKeeper from './components/Notes_Keeper/NotesKeeper';
+
+import Index from "./components/LandingPage/Index";
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from 'react-dom/client';
+
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar/>
+      <BrowserRouter>
+          <Routes>
+              <Route exact path="/" element={<Index />} />
+              <Route exact path="/trivia" element={<Trivia  />} />
+              <Route exact path="/account" element={<Dashboard  />} />
+              <Route exact path="/noteskeeper" element={<NotesKeeper  />} />
+            {/* <TriviaQuizPanel/> */}
+            {/* <CourseDetails/> */}
+          </Routes>
+        </BrowserRouter>
+      <Footer/>
+    </ChakraProvider>
+  )
+}
+
+export default App;
---
+++ Frontend/src/components/LandingPage/Index.js
@@ -0,0 +1,11 @@
+import ImageSlider from "./ImageSlider";
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ImageSlider />
+);
+
+ export default Index 
