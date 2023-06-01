6966d153958ac4c2c70847d3a823bad2eddf3f08
anushka-srivastava22 <srivastavaanushka3@gmail.com>
2023-06-02 00:37:51 +0530

    Added MongoDB Database Code

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,39 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
 
-const DB = 'mongodb+srv://srivastavaanushka3:libermajorproject@cluster0.7nnisuo.mongodb.net/liber?retryWrites=true&w=majority';
+const DB = 'mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true';
 
 
-mongoose.connect(DB, {
-  userNewUrlParser:true,
-  userCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindandModify: false
-}).then(() => {
-  console.log(`connection successful`);
-}).catch((err) => console.log(`no connection`));
+async function main(){
+  
+  const uri = "mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+// Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 const middleware = (req,res,next) => {
   console.log(`Hello middleware`);
