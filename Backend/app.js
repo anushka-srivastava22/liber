6c498fa0b83fa9cd4538aed4cf5060f4142c8530
anushka-srivastava22 <srivastavaanushka3@gmail.com>
2023-06-04 02:20:55 +0530

    Added bckend for signup page

--- Backend/app.js
+++ Backend/app.js
@@ -1,39 +1,42 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+app.use(express.json());
+//const { MongoClient } = require('mongodb');
 
-const DB = 'mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true';
+const uri = "mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true&w=majority";
 
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+}).then(() =>{
+  console.log("Connect to Database");
+}).catch((e) => console.log(e));
 
-async function main(){
-  
-  const uri = "mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true&w=majority";
+require("./userDetails");
 
+const User = mongoose.model("users");
 
-  const client = new MongoClient(uri);
-
+app.post("/register", async (req, res) => {
+  const { fname,email, password } = req.body;
+  console.log(fname, email, password);
+  const encryptedPassword = await bcrypt.hash(password, 10);
   try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-// Make the appropriate DB calls
-      await  listDatabases(client);
+    const oldUser = await User.findOne({ email });
 
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
+    if (oldUser) {
+      return res.json({ error: "User Exists" });
+    }
+    await User.create({
+      fname,
+      email,
+      password: encryptedPassword,
+      
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
   }
-}
-
-main().catch(console.error);
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+});
 
 const middleware = (req,res,next) => {
   console.log(`Hello middleware`);
@@ -62,3 +65,55 @@ app.get('/contact', (req,res) => {
 app.listen(3000,() => {
   console.log(`server running at port 3000`);
 })
+
+
+/*async function main(){
+  
+  const uri = "mongodb+srv://srivastavaanushka3:LiberMajorProject@cluster0.xbteq4v.mongodb.net/Liber?retryWrites=true&w=majority";
+
+
+ const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+// Make the appropriate DB calls
+    await  listDatabases(client);
+    console.log("")
+    await listCollections(client)
+    console.log("")
+    await listData(client);
+
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function listCollections(client){
+  collectionList = await client.db("Liber").listCollections().toArray();
+
+  console.log("Collections in Liber:");
+  collectionList.forEach(collection => console.log(` - ${collection.name}`));
+
+  return collectionList
+};
+async function listData(client){
+  dataList = await client.db("Liber").collection("users").find({}).toArray();
+
+  console.log("DataList in Liber:");
+  console.log(dataList)
+};
+*/
--- Frontend/src/components/Login_Signup/Signup.js
+++ Frontend/src/components/Login_Signup/Signup.js
@@ -14,8 +14,56 @@ import {
     InputGroup,
     Heading,
   } from "@chakra-ui/react";
+  import React, { useState } from "react";
   
-  export default function Signup() {
+  export default function Signup(){
+
+    /*constructor(props)
+    {
+      super(props)
+      this.state={
+        fname:"",
+        email:"",
+        password:""
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }*/
+    const [fname, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+      
+        e.preventDefault();
+  
+        console.log(fname, email, password);
+        fetch("http://localhost:3001/register", {
+          method: "POST",
+          crossDomain: true,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            fname,
+            email,
+            password,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data, "userRegister");
+            if (data.status == "ok") {
+              alert("Registration Successful");
+            } else {
+              alert("Something went wrong");
+            }
+          });
+      
+    };
+  
+
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
       <>
@@ -28,7 +76,7 @@ import {
           Sign Up
         </Button>
   
-        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} >
         <ModalOverlay />
         <ModalContent
           h="80vh"
@@ -51,7 +99,7 @@ import {
                   pointerEvents="none"
                   children={<i className="fas fa-signature"></i>}
                 />
-                <Input type={"text"} placeholder="Name"/>
+                <Input type={"text"} placeholder="Name" onChange={event => setName(event.currentTarget.value)} />
               </InputGroup>
             </Box>
             <Box pt="3">
@@ -60,7 +108,7 @@ import {
                   pointerEvents="none"
                   children={<i className="fas fa-at"></i>}
                 />
-                <Input type={"email"} placeholder="E-mail ID" />
+                <Input type={"email"} placeholder="E-mail ID" onChange={event => setEmail(event.currentTarget.value)} />
               </InputGroup>
             </Box>
             <Box pt="3">
@@ -69,12 +117,12 @@ import {
                   pointerEvents="none"
                   children={<i className="fas fa-key"></i>}
                 />
-                <Input type={"password"} placeholder="Password" />
+                <Input type={"password"} placeholder="Password" onChange={event => setPassword(event.currentTarget.value)} />
               </InputGroup>
             </Box>
             <Box pt="3">
               <InputGroup>
-                <Button type="submit" bg='#ecc145' w={"100%"}>Signup</Button>
+                <Button type="submit" bg='#ecc145' w={"100%"} onClick={handleSubmit}>Signup</Button>
               </InputGroup>
             </Box>
             <Box pt="8" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
@@ -91,6 +139,7 @@ import {
                 bottom: "0",
                 left: "50%",
                 transform: "translateX(-50%)",
+
               }}
             />
           </ModalFooter>
